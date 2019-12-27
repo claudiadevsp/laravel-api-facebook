a00cc7dd5cee985aac4198b79c84c65508a96a3a
Claudia <claudia.devsp@gmail.com>
2019-12-27 17:16:19 -0300

    first commit

---
+++ app/Http/Controllers/GraphController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+ 
+use Facebook\Exceptions\FacebookSDKException;
+use Facebook\Facebook;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+ 
+class GraphController extends Controller
+{
+    private $api;
+    public function __construct(Facebook $fb)
+    {
+        $this->middleware(function ($request, $next) use ($fb) {
+            $fb->setDefaultAccessToken(Auth::user()->token);
+            $this->api = $fb;
+            return $next($request);
+        });
+    }
+ 
+    public function retrieveUserProfile(){
+        try {
+ 
+            $params = "first_name,last_name,age_range,gender";
+ 
+            $user = $this->api->get('/me?fields='.$params)->getGraphUser();
+ 
+            dd($user);
+ 
+        } catch (FacebookSDKException $e) {
+ 
+        }
+ 
+    }
+}
