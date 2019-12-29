fe1844af4e2aee24c6ede9b92a0a2eb44f6dddea
Claudia <claudia.devsp@gmail.com>
2019-12-29 08:50:10 -0300

    alter config redirect

--- config/services.php
+++ config/services.php
@@ -36,7 +36,7 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_APP_ID'),         
         'client_secret' => env('FACEBOOK_APP_SECRET'),
-        'redirect' => 'http://localhost:8000/login/facebook/callback'     
+        'redirect' => env('FACEBOOK_APP_REDIRECT')     
     ],
 
 ];
